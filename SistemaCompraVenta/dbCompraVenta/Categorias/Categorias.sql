71ec49b1d87ff2647dd884998c71a8137c9fb4b0
Sebastián Serrisuela <sebaserri@gmail.com>
2019-06-30 18:24:25 -0300

    Creada tabla Categorias. #1

---
+++ SistemaCompraVenta/dbCompraVenta/Categorias/Categorias.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Categorias](
+	[Id] [int] NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[Descripcion] [varchar](255) NULL,
+	[Estado] [bit] NOT NULL,
+ CONSTRAINT [PK_Categorias] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Categorias] ADD  CONSTRAINT [DF_Categorias_Descripcion]  DEFAULT ('') FOR [Descripcion]
+GO
+
+ALTER TABLE [dbo].[Categorias] ADD  CONSTRAINT [DF_Categorias_Estado]  DEFAULT ((1)) FOR [Estado]
+GO
