5d4f4269ad9985f0e83135cb1cb01786ef33321b
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-02 19:13:32 -0300

    Creada tabla Roles. #1

---
+++ SistemaCompraVenta/dbCompraVenta/Roles/Roles.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Roles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](30) NOT NULL,
+	[Descripcion] [varchar](255) NULL,
+	[Estado] [bit] NOT NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Roles] ADD  CONSTRAINT [DF_Roles_Descripcion]  DEFAULT ('') FOR [Descripcion]
+GO
+
+ALTER TABLE [dbo].[Roles] ADD  CONSTRAINT [DF_Roles_Estado]  DEFAULT ((1)) FOR [Estado]
+GO
