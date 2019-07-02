27a50927a499666f7b08427a522feec196c81564
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-02 20:50:20 -0300

    Creada tabla Ingresos_Detalle. #1

---
+++ SistemaCompraVenta/dbCompraVenta/Ingresos/Ingresos_Detalle.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[Ingresos_Detalle](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IngresoId] [int] NOT NULL,
+	[ArticuloId] [int] NOT NULL,
+	[Cantidad] [int] NOT NULL,
+	[Precio] [decimal](11, 2) NOT NULL,
+ CONSTRAINT [PK_Ingresos_Detalle] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Ingresos_Detalle] ADD  CONSTRAINT [FK_Ingresos_Detalle_Articulos] FOREIGN KEY([ArticuloId])
+REFERENCES [dbo].[Articulos] ([Id])
+GO
+
+ALTER TABLE [dbo].[Ingresos_Detalle] CHECK CONSTRAINT [FK_Ingresos_Detalle_Articulos]
+GO
+
+ALTER TABLE [dbo].[Ingresos_Detalle] ADD  CONSTRAINT [FK_Ingresos_Detalle_Ingresos] FOREIGN KEY([IngresoId])
+REFERENCES [dbo].[Ingresos] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[Ingresos_Detalle] CHECK CONSTRAINT [FK_Ingresos_Detalle_Ingresos]
+GO
