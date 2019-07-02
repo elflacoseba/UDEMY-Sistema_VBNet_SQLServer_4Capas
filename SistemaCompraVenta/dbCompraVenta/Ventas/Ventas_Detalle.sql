4d15e9fcc0cc9eba3466a7ee24b4151cb6cb402d
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-02 20:59:48 -0300

    Creada tabla Ventas_Detalle. #1

---
+++ SistemaCompraVenta/dbCompraVenta/Ventas/Ventas_Detalle.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [dbo].[Ventas_Detalle](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[VentaId] [int] NOT NULL,
+	[ArticuloId] [int] NOT NULL,
+	[Cantidad] [int] NOT NULL,
+	[Precio] [decimal](11, 2) NOT NULL,
+	[Descuento] [decimal](11, 2) NOT NULL,
+ CONSTRAINT [PK_Ventas_Detalle] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Ventas_Detalle] ADD CONSTRAINT [FK_Ventas_Detalle_Articulos] FOREIGN KEY([ArticuloId])
+REFERENCES [dbo].[Articulos] ([Id])
+GO
+
+ALTER TABLE [dbo].[Ventas_Detalle] CHECK CONSTRAINT [FK_Ventas_Detalle_Articulos]
+GO
+
+ALTER TABLE [dbo].[Ventas_Detalle] ADD  CONSTRAINT [FK_Ventas_Detalle_Ventas] FOREIGN KEY([VentaId])
+REFERENCES [dbo].[Ventas] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[Ventas_Detalle] CHECK CONSTRAINT [FK_Ventas_Detalle_Ventas]
+GO
