16a348672b514407b740e2c141d6eb301a0f48e8
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 15:18:55 -0300

    Creado SP Categorias_Listar #5

---
+++ SistemaCompraVenta/dbCompraVenta/Categorias/Categorias_Listar.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 07/07/2019
+-- Description:	Lista las categorías
+-- =============================================
+CREATE PROCEDURE Categorias_Listar
+AS
+BEGIN
+
+  SELECT [Id]
+        ,[Nombre]
+        ,[Descripcion]
+        ,[Estado]
+  FROM [dbo].[Categorias]
+  ORDER BY Nombre
+
+  RETURN 0
+END
