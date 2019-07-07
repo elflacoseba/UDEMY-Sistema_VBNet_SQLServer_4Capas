bd3d491b7433583eb7c36ba761d5e13aa44b85e4
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 17:15:02 -0300

    Creado SP Categorias_Desactivar. #5

---
+++ SistemaCompraVenta/dbCompraVenta/Categorias/Categorias_Desactivar.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 07/07/2019
+-- Description: Desactiva una Categoría de la Base de Datos.
+-- =============================================
+CREATE PROCEDURE Categorias_Desactivar
+	@Id INT
+AS
+BEGIN
+
+UPDATE [dbo].[Categorias]
+   SET [Estado] = 0
+ WHERE (Id = @Id)
+
+--SI MODIFICÓ UNA FILA RETORNO 1, SINO RETORNO 0
+IF (@@ROWCOUNT = 1)
+	RETURN 1
+ELSE
+	RETURN 0
+
+END
