e9188b2198773a111a01885bbe9f239dcaa4a2f3
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 17:16:10 -0300

    Creado SP Categorias_Activar. #5

---
+++ SistemaCompraVenta/dbCompraVenta/Categorias/Categorias_Activar.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 07/07/2019
+-- Description: Activa una Categoría de la Base de Datos.
+-- =============================================
+CREATE PROCEDURE Categorias_Activar
+	@Id INT
+AS
+BEGIN
+
+UPDATE [dbo].[Categorias]
+   SET [Estado] = 1
+ WHERE (Id = @Id)
+
+--SI MODIFICÓ UNA FILA RETORNO 1, SINO RETORNO 0
+IF (@@ROWCOUNT = 1)
+	RETURN 1
+ELSE
+	RETURN 0
+
+END
