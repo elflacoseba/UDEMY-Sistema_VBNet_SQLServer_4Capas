ac49218b3962b56305cd75dcd4d9d4a615aaf76c
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 17:39:33 -0300

    Creada función Buscar en la clase de acceso a datos. #6

--- SistemaCompraVenta/Datos/Categorias.vb
+++ SistemaCompraVenta/Datos/Categorias.vb
@@ -3,6 +3,10 @@
 Public Class Categorias
     Inherits Conexion
 
+    ''' <summary>
+    ''' Obtiene todas las Categorías de la base de datos.
+    ''' </summary>
+    ''' <returns></returns>
     Public Function Listar() As DataTable
 
         Dim Resultados As SqlDataReader
@@ -12,6 +16,45 @@ Public Class Categorias
             Using cmd As New SqlCommand("Categorias_Listar", MyBase.Cnn)
 
                 cmd.CommandType = CommandType.StoredProcedure
+                MyBase.Cnn.Open()
+
+                Resultados = cmd.ExecuteReader()
+
+                Using Tabla As New DataTable
+                    Tabla.BeginLoadData()
+                    Tabla.Load(Resultados)
+                    Tabla.EndLoadData()
+
+                    Return Tabla
+
+                End Using
+
+            End Using
+
+        Catch ex As Exception
+            Throw ex
+        Finally
+            MyBase.Cnn.Close()
+            Resultados = Nothing
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Buscar Categorías por nombre y descripción.
+    ''' </summary>
+    ''' <param name="Valor">Valor a buscar.</param>
+    ''' <returns></returns>
+    Public Function Buscar(Valor As String) As DataTable
+
+        Dim Resultados As SqlDataReader
+
+        Try
+
+            Using cmd As New SqlCommand("Categorias_Buscar", MyBase.Cnn)
+
+                cmd.CommandType = CommandType.StoredProcedure
+                cmd.Parameters.AddWithValue("@Valor", Valor)
+
                 MyBase.Cnn.Open()
 
                 Resultados = cmd.ExecuteReader()
