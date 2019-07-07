6c2871c663eced171b6e5db645b1047a405330dd
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 17:12:56 -0300

    Creado SP Categorias_Eliminar. #5

---
+++ SistemaCompraVenta/dbCompraVenta/Categorias/Categorias_Eliminar.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 07/07/2019
+-- Description: Elimina una Categoría de la Base de Datos.
+-- =============================================
+CREATE PROCEDURE Categorias_Eliminar
+	@Id INT
+AS
+BEGIN
+
+DELETE FROM [dbo].[Categorias]
+      WHERE (Id = @Id)
+
+--SI ELIMINÓ UNA FILA RETORNO 1, SINO RETORNO 0
+IF (@@ROWCOUNT = 1)
+	RETURN 1
+ELSE
+	RETURN 0
+
+END
