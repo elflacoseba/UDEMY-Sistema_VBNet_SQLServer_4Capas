73310f58bbf3be0f1eba2bbf0232756c284816d7
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 17:57:55 -0300

    Creada función Buscar en Negocios Categorías. #7

--- SistemaCompraVenta/Negocios/Categorias.vb
+++ SistemaCompraVenta/Negocios/Categorias.vb
@@ -26,4 +26,32 @@
             Tabla = Nothing
         End Try
     End Function
+
+    ''' <summary>
+    ''' Busca Categorías por nombre y descripción.
+    ''' </summary>
+    ''' <param name="Valor"></param>
+    ''' <returns></returns>
+    Public Function Buscar(Valor As String) As DataTable
+
+        Dim oDatos As Datos.Categorias
+        Dim Tabla As DataTable
+
+        Try
+            oDatos = New Datos.Categorias()
+
+            Tabla = New DataTable
+
+            Tabla = oDatos.Buscar(Valor)
+
+            Return Tabla
+
+        Catch ex As Exception
+            Throw ex
+            Return Nothing
+        Finally
+            oDatos = Nothing
+            Tabla = Nothing
+        End Try
+    End Function
 End Class
