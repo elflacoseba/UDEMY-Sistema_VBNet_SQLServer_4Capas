2407a01b3f6bad29afee914f1c19605bd6526bed
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 15:29:45 -0300

    Creado SP Categorias_Buscar #5

---
+++ SistemaCompraVenta/dbCompraVenta/Categorias/Categorias_Buscar.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 07/07/2019
+-- Description:	Busca en el nombre y en la descripción de las categorías
+-- =============================================
+CREATE PROCEDURE Categorias_Buscar
+	@Valor VARCHAR(50)
+AS
+BEGIN
+
+SELECT [Id]
+      ,[Nombre]
+      ,[Descripcion]
+      ,[Estado]
+  FROM [dbo].[Categorias]
+  WHERE (Nombre LIKE '%' + @Valor + '%') OR (Descripcion LIKE '%' + @Valor + '%')
+  ORDER BY Nombre
+
+  RETURN 0
+END
