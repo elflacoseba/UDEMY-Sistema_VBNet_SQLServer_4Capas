c163dccb9107f471fbbfd75f4afa0fa1e5e04780
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 17:56:17 -0300

    -Creada clase Negocios Categorias. -Creada función Listar en Negocios Categorias. #7

---
+++ SistemaCompraVenta/Negocios/Categorias.vb
@@ -0,0 +1,29 @@
+﻿Public Class Categorias
+
+    ''' <summary>
+    ''' Devuelve un listado con todas las Categorías de la base de datos.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function Listar() As DataTable
+
+        Dim oDatos As Datos.Categorias
+        Dim Tabla As DataTable
+
+        Try
+            oDatos = New Datos.Categorias()
+
+            Tabla = New DataTable
+
+            Tabla = oDatos.Listar()
+
+            Return Tabla
+
+        Catch ex As Exception
+            Throw ex
+            Return Nothing
+        Finally
+            oDatos = Nothing
+            Tabla = Nothing
+        End Try
+    End Function
+End Class
