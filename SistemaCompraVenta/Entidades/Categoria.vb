c11b1220694384110d4e63d88339dd4b6c59024a
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-07 15:02:06 -0300

    Creada Entidad Categoría. Closed #4

---
+++ SistemaCompraVenta/Entidades/Categoria.vb
@@ -0,0 +1,52 @@
+﻿Public Class Categoria
+
+#Region "Campos"
+
+    Private _CategoriaID As Integer
+    Private _Nombre As String
+    Private _Descripcion As String
+    Private _Estado As Boolean
+
+#End Region
+
+#Region "Propiedades"
+
+    Public Property CategoriaID As Integer
+        Get
+            Return _CategoriaID
+        End Get
+        Set(value As Integer)
+            _CategoriaID = value
+        End Set
+    End Property
+
+    Public Property Nombre As String
+        Get
+            Return _Nombre
+        End Get
+        Set(value As String)
+            _Nombre = value
+        End Set
+    End Property
+
+    Public Property Descripcion As String
+        Get
+            Return _Descripcion
+        End Get
+        Set(value As String)
+            _Descripcion = value
+        End Set
+    End Property
+
+    Public Property Estado As Boolean
+        Get
+            Return _Estado
+        End Get
+        Set(value As Boolean)
+            _Estado = value
+        End Set
+    End Property
+
+#End Region
+
+End Class
