cba7b3559822178bcf68850d3301c407270772b8
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-09 18:51:00 -0300

    Programado, listar Categorías. #9 #10

--- SistemaCompraVenta/SistemaCompraVenta/frmCategorias.Designer.vb
+++ SistemaCompraVenta/SistemaCompraVenta/frmCategorias.Designer.vb
@@ -22,16 +22,130 @@ Partial Class frmCategorias
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TabControlCategorias = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.pnlGrilla = New System.Windows.Forms.Panel()
+        Me.dgvListadoCategorias = New System.Windows.Forms.DataGridView()
+        Me.colSeleccionar = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.pnlPie = New System.Windows.Forms.Panel()
+        Me.lblTotalCategorias = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabControlCategorias.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.pnlGrilla.SuspendLayout()
+        CType(Me.dgvListadoCategorias, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlPie.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'TabControlCategorias
+        '
+        Me.TabControlCategorias.Controls.Add(Me.TabPage1)
+        Me.TabControlCategorias.Controls.Add(Me.TabPage2)
+        Me.TabControlCategorias.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControlCategorias.Location = New System.Drawing.Point(0, 0)
+        Me.TabControlCategorias.Name = "TabControlCategorias"
+        Me.TabControlCategorias.SelectedIndex = 0
+        Me.TabControlCategorias.Size = New System.Drawing.Size(800, 450)
+        Me.TabControlCategorias.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.pnlGrilla)
+        Me.TabPage1.Controls.Add(Me.pnlPie)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(792, 421)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Listado"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'pnlGrilla
+        '
+        Me.pnlGrilla.Controls.Add(Me.dgvListadoCategorias)
+        Me.pnlGrilla.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlGrilla.Location = New System.Drawing.Point(3, 3)
+        Me.pnlGrilla.Name = "pnlGrilla"
+        Me.pnlGrilla.Size = New System.Drawing.Size(786, 385)
+        Me.pnlGrilla.TabIndex = 1
+        '
+        'dgvListadoCategorias
+        '
+        Me.dgvListadoCategorias.AllowUserToAddRows = False
+        Me.dgvListadoCategorias.AllowUserToDeleteRows = False
+        Me.dgvListadoCategorias.AllowUserToOrderColumns = True
+        Me.dgvListadoCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvListadoCategorias.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colSeleccionar})
+        Me.dgvListadoCategorias.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvListadoCategorias.Location = New System.Drawing.Point(0, 0)
+        Me.dgvListadoCategorias.Name = "dgvListadoCategorias"
+        Me.dgvListadoCategorias.ReadOnly = True
+        Me.dgvListadoCategorias.RowHeadersWidth = 51
+        Me.dgvListadoCategorias.RowTemplate.Height = 24
+        Me.dgvListadoCategorias.Size = New System.Drawing.Size(786, 385)
+        Me.dgvListadoCategorias.TabIndex = 1
+        '
+        'colSeleccionar
+        '
+        Me.colSeleccionar.Frozen = True
+        Me.colSeleccionar.HeaderText = "Seleccionar"
+        Me.colSeleccionar.MinimumWidth = 6
+        Me.colSeleccionar.Name = "colSeleccionar"
+        Me.colSeleccionar.ReadOnly = True
+        Me.colSeleccionar.Width = 125
+        '
+        'pnlPie
+        '
+        Me.pnlPie.BackColor = System.Drawing.Color.Transparent
+        Me.pnlPie.Controls.Add(Me.lblTotalCategorias)
+        Me.pnlPie.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnlPie.Location = New System.Drawing.Point(3, 388)
+        Me.pnlPie.Name = "pnlPie"
+        Me.pnlPie.Size = New System.Drawing.Size(786, 30)
+        Me.pnlPie.TabIndex = 0
+        '
+        'lblTotalCategorias
+        '
+        Me.lblTotalCategorias.Location = New System.Drawing.Point(550, 5)
+        Me.lblTotalCategorias.Name = "lblTotalCategorias"
+        Me.lblTotalCategorias.Size = New System.Drawing.Size(231, 17)
+        Me.lblTotalCategorias.TabIndex = 0
+        Me.lblTotalCategorias.Text = "Total de registros:"
+        Me.lblTotalCategorias.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(792, 421)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Mantenimiento"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'frmCategorias
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TabControlCategorias)
         Me.Name = "frmCategorias"
         Me.Text = "Categorías"
+        Me.TabControlCategorias.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.pnlGrilla.ResumeLayout(False)
+        CType(Me.dgvListadoCategorias, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlPie.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents TabControlCategorias As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents pnlGrilla As Panel
+    Friend WithEvents dgvListadoCategorias As DataGridView
+    Friend WithEvents colSeleccionar As DataGridViewCheckBoxColumn
+    Friend WithEvents pnlPie As Panel
+    Friend WithEvents lblTotalCategorias As Label
 End Class
--- SistemaCompraVenta/SistemaCompraVenta/frmCategorias.vb
+++ SistemaCompraVenta/SistemaCompraVenta/frmCategorias.vb
@@ -1,3 +1,36 @@
 ﻿Public Class frmCategorias
 
+    Private Sub frmCategorias_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Listar()
+    End Sub
+
+    Private Sub Formato()
+        With dgvListadoCategorias
+            .Columns(0).Visible = False
+            .Columns(0).Width = 50
+            .Columns(1).Width = 50
+            .Columns(1).HeaderText = "ID"
+            .Columns(1).HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight
+            .Columns(1).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+            .Columns(2).Width = 150
+            .Columns(3).Width = 250
+            .Columns(3).HeaderText = "Descripción"
+            .Columns(4).Width = 84
+            .Columns(4).HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+        End With
+    End Sub
+
+    Private Sub Listar()
+        Dim oNegCat As Negocios.Categorias
+        Try
+            oNegCat = New Negocios.Categorias
+            dgvListadoCategorias.DataSource = oNegCat.Listar()
+            lblTotalCategorias.Text = "Total de registros: " & dgvListadoCategorias.Rows.Count().ToString()
+            Formato()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            oNegCat = Nothing
+        End Try
+    End Sub
 End Class
