c7c516c8373b356dddebfba5caa5b0d66ba08489
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-14 16:18:33 -0300

    Creado SP Articulos_Insertar. # 16

---
+++ SistemaCompraVenta/dbCompraVenta/Articulos/Articulos_Insertar.sql
@@ -0,0 +1,35 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 14/07/2019
+-- Description:	Inserta un nuevo Artículo en la base de datos.
+-- =============================================
+CREATE PROCEDURE Articulos_Insertar
+	@CategoriaId int,
+    @Codigo varchar(50),
+    @Nombre varchar(100),
+    @Precio_Venta decimal(11,2),
+    @Stock int,
+    @Descripcion varchar(255),
+    @Imagen varchar(255)
+AS
+BEGIN
+
+INSERT INTO [dbo].[Articulos]
+           ([CategoriaId]
+           ,[Codigo]
+           ,[Nombre]
+           ,[Precio_Venta]
+           ,[Stock]
+           ,[Descripcion]
+           ,[Imagen])
+     VALUES
+           (@CategoriaID
+           ,@Codigo
+           ,@Nombre
+           ,@Precio_Venta
+           ,@Stock
+           ,@Descripcion
+           ,@Imagen)
+
+  RETURN @@ERROR
+END
