6e9d008ac2d145a09cc18ed5fe9fb4e9b024e270
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-14 16:26:57 -0300

    Creado SP Articulos_Desactivar. #16

---
+++ SistemaCompraVenta/dbCompraVenta/Articulos/Articulos_Desactivar.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 14/07/2019
+-- Description:	Desactiva un Artículo de la base de datos.
+-- =============================================
+CREATE PROCEDURE Articulos_Desactivar
+	@ArticuloID int
+AS
+BEGIN
+
+UPDATE [dbo].[Articulos]
+SET Estado = 0
+WHERE Id = @ArticuloID
+
+--SI DESACTIVÓ UNA FILA RETORNO 1, SINO RETORNO 0
+IF (@@ROWCOUNT = 1)
+	RETURN 1
+ELSE
+	RETURN 0
+
+END
