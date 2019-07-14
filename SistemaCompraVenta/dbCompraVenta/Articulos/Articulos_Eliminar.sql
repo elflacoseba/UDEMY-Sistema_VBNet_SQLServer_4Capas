d4fce9e814eff778053c5986c66cad0ea5036143
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-14 16:25:09 -0300

    Creado SP Articulos_Eliminar. #16

---
+++ SistemaCompraVenta/dbCompraVenta/Articulos/Articulos_Eliminar.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 14/07/2019
+-- Description:	Elimina un Artículo de la base de datos.
+-- =============================================
+CREATE PROCEDURE Articulos_Eliminar
+	@ArticuloID int
+AS
+BEGIN
+
+DELETE FROM [dbo].[Articulos]
+      WHERE Id = @ArticuloID
+
+--SI ELIMINÓ UNA FILA RETORNO 1, SINO RETORNO 0
+IF (@@ROWCOUNT = 1)
+	RETURN 1
+ELSE
+	RETURN 0
+
+END
