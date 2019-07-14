29b980b07a2769b77d2a4e3d9580f9e52a0adf04
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-14 16:12:59 -0300

    Creado SP Articulos_Buscar. #16

---
+++ SistemaCompraVenta/dbCompraVenta/Articulos/Articulos_Buscar.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 14/07/2019
+-- Description:	Busca Artículos por Código, Nombre y Descripción..
+-- =============================================
+CREATE PROCEDURE Articulos_Buscar
+	@Valor VARCHAR(50)
+AS
+BEGIN
+
+SELECT	 A.Id AS ArticuloID, C.Id AS CategoriaID, C.Nombre AS CategoriaNombre
+		,A.Codigo AS ArticuloCodigo, A.Nombre AS ArticuloNombre, a.Precio_Venta AS ArticuloPrecioVenta
+		,A.Stock AS ArticuloStock, A.Descripcion AS ArticuloDescripcion, A.Estado AS ArticuloEstado
+  FROM [dbo].[Articulos] AS A INNER JOIN
+  [dbo].[Categorias] AS C ON C.Id = A.CategoriaId
+  WHERE		A.Codigo LIKE '%' + @Valor + '%'
+		 OR A.Nombre LIKE '%' + @Valor + '%'
+		 OR A.Descripcion LIKE '%' + @Valor + '%'
+  ORDER BY A.Nombre ASC
+
+  RETURN 0
+END
