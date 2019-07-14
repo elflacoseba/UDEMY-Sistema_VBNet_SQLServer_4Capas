292d3ec553b96658f3f7cb531306519ce675913d
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-14 16:27:50 -0300

    Creado SP Articulos_Activar. #16

---
+++ SistemaCompraVenta/dbCompraVenta/Articulos/Articulos_Activar.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 14/07/2019
+-- Description:	Activa un Artículo de la base de datos.
+-- =============================================
+CREATE PROCEDURE Articulos_Activar
+	@ArticuloID int
+AS
+BEGIN
+
+UPDATE [dbo].[Articulos]
+SET Estado = 1
+WHERE Id = @ArticuloID
+
+--SI ACTIVÓ UNA FILA RETORNO 1, SINO RETORNO 0
+IF (@@ROWCOUNT = 1)
+	RETURN 1
+ELSE
+	RETURN 0
+
+END
