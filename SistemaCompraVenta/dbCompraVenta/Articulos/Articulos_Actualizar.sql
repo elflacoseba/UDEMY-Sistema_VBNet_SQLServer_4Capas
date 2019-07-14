6886eec94411410a6d3f52c70c8d0fa7b430cbbc
Sebastián Serrisuela <sebaserri@gmail.com>
2019-07-14 16:22:54 -0300

    Creado SP Articulos_Actualizar. #16

---
+++ SistemaCompraVenta/dbCompraVenta/Articulos/Articulos_Actualizar.sql
@@ -0,0 +1,34 @@
+﻿-- =============================================
+-- Author:		Sebastián Serrisuela
+-- Create date: 14/07/2019
+-- Description:	Actualiza un Artículo de la base de datos.
+-- =============================================
+CREATE PROCEDURE Articulos_Actualizar
+	@ArticuloID int,
+	@CategoriaId int,
+    @Codigo varchar(50),
+    @Nombre varchar(100),
+    @Precio_Venta decimal(11,2),
+    @Stock int,
+    @Descripcion varchar(255),
+    @Imagen varchar(255)
+AS
+BEGIN
+
+UPDATE [dbo].[Articulos]
+   SET [CategoriaId] = @CategoriaId
+      ,[Codigo] = @Codigo
+      ,[Nombre] = @Nombre
+      ,[Precio_Venta] = @Precio_Venta
+      ,[Stock] = @Stock
+      ,[Descripcion] = @Descripcion
+      ,[Imagen] = @Imagen
+ WHERE (Id = @ArticuloID)
+
+--SI MODIFICÓ UNA FILA RETORNO 1, SINO RETORNO 0
+IF (@@ROWCOUNT = 1)
+	RETURN 1
+ELSE
+	RETURN 0
+
+END
